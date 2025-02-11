6dfaacf99dd24704fa2fe87d28408f4d398aba92
ryanchall99 <rileychall@hotmail.co.uk>
2025-02-11 16:04:33 +0000

    Firing Shoot Animation
    - Animation now plays each time the shoot button is pressed.

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,12 +4,23 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] ParticleSystem MuzzleFlash;
 
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponentInParent<Animator>();
+    }
+
     public void Shoot(WeaponSO weaponSO)
     {
         RaycastHit hit;
 
         MuzzleFlash.Play();
+        _animator.Play(AnimationNames.Shoot, 0, 0f); // Animation / Layer / Start Frame
+
+        /* --- DEBUGGING (REMOVE LATER) --- */
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * weaponSO.Range, Color.red, 30f);
+        /* ---------------------------------*/
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, weaponSO.Range))
         {
