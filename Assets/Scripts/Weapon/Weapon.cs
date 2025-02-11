2b9885092908730b20b6d799ddd75a956ef539bc
ryanchall99 <rileychall@hotmail.co.uk>
2025-02-11 17:47:14 +0000

    Hit VFX
    - Particle system for the Hit VFX, will be instantiated on Raycast hit.

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] ParticleSystem MuzzleFlash;
+    [SerializeField] ParticleSystem HitVFX;
 
     private Animator _animator;
 
