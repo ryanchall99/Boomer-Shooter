e4ca148f8f1e9b54f1829fbb345393017fc49469
ryanchall99 <rileychall@hotmail.co.uk>
2025-02-14 15:45:15 +0000

    Machine Gun Prefab

--- Assets/Scripts/Weapon/ActiveWeapon.cs
+++ Assets/Scripts/Weapon/ActiveWeapon.cs
@@ -7,17 +7,27 @@ public class ActiveWeapon : MonoBehaviour
     private Animator _animator;
     Weapon _currentWeapon;
 
+    float _timeSinceLastShot = 0f;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
         _currentWeapon = GetComponentInChildren<Weapon>();
     }
 
+    void Update()
+    {
+        _timeSinceLastShot += Time.deltaTime; // Adding time every frame.
+    }
 
     public void HandleShoot()
     {
-        _currentWeapon.Shoot(weaponSO);
-
-        _animator.Play(AnimationNames.Shoot, 0, 0f); // Animation / Layer / Start Frame
+        if (_timeSinceLastShot >= weaponSO.FireRate)
+        {
+            _currentWeapon.Shoot(weaponSO);
+            _animator.Play(AnimationNames.Shoot, 0, 0f); // Animation / Layer / Start Frame
+            
+            _timeSinceLastShot = 0f; // Reset back to 0
+        }
     }
 }
