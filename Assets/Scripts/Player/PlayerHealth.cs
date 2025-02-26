71409a482124e4a44eb133f3aab0cf20cb13a3ff
ryanchall99 <rileychall@hotmail.co.uk>
2025-02-26 17:22:42 +0000

    Death Camera Transition

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,17 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : BaseHealth
 {
+    [SerializeField] CinemachineCamera DeathVirtualCamera;
+    [SerializeField] Transform WeaponCamera;
+
+    int _gameOverCameraPriority = 20;
+
     public override void Death()
     {
+        WeaponCamera.parent = null;
+        DeathVirtualCamera.Priority = _gameOverCameraPriority;
         Destroy(this.gameObject);
     }
 
